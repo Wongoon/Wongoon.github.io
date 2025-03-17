a87b09487ce4a88d3018f48b8a7a52a4d31d5034
Wongoon <rmsgh64@gmail.com>
2025-03-17 23:37:48 +0900

    Update: `Design`

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+const links = document.querySelectorAll('.contents span');
+const indicator = document.getElementById('indicator');
+
+function Indicator(e) {
+    indicator.style.top = e.offsetTop + 'px';
+    indicator.style.width = (e.offsetWidth + 10) + 'px';
+    links.forEach(link => {
+        link.classList.remove('current');
+    });
+    e.classList.add('current');
+}
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        if (window.confirm("이동하시겠습니까?"))
+            window.open("https://wongoon.github.io/" + link.getAttribute("id"));
+    });
+
+    link.addEventListener('mousemove', (e) => {
+        Indicator(e.target);
+    });
+});
